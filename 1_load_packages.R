6dd2861f57b7df0d93185e6f4586b5a1c8f43c8a
Han Lin <HanLin.Shang@gmail.com>
2026-02-26 23:16:11 +1100

    Update 1_load_packages.R

--- 1_load_packages.R
+++ 1_load_packages.R
@@ -1,3 +1,21 @@
+################
+# name packages
+################
+
+packages <- c("ftsa", "sde", "hdftsa", "xtable", "dplyr", "quantreg") 
+
+## Now load or install and load all 
+
+package_check <- lapply(
+  packages,
+  FUN <- function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
 require(hdftsa)
 require(sde)
 require(ftsa)
